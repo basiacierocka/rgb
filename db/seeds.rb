91747310d1a4ccf70406781ee4e75cbf64128e27
Basia <basiacierocka@gmail.com>
2016-05-22 08:29:07 +0000

    initial commit

---
+++ db/seeds.rb
@@ -0,0 +1,4 @@
+User.destroy_all
+User.create(first_name: "Barbara", last_name: "Cierocka", age: 30)
+User.create(first_name: "Paula", last_name: "Bojar", age: 21)
+User.create(first_name: "Monika", last_name: "Otto", age: 21)
