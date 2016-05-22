6750724e90f60dbdeed741debd669c71eddcb6f1
Basia <basiacierocka@gmail.com>
2016-05-22 10:20:47 +0000

    Add cars to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,16 @@
 User.destroy_all
-User.create(first_name: "Barbara", last_name: "Cierocka", age: 30)
-User.create(first_name: "Paula", last_name: "Bojar", age: 21)
-User.create(first_name: "Monika", last_name: "Otto", age: 21)
+user_1 = User.create!(first_name: "Barbara", last_name: "Cierocka", age: 30)
+user_2 = User.create!(first_name: "Paula", last_name: "Bojar", age: 21)
+user_3 = User.create!(first_name: "Monika", last_name: "Otto", age: 21)
+
+Car.destroy_all
+car_1 = Car.create(color: "red", user: user_1)
+car_2 = Car.create(color: "black", user: user_1)
+car_3 = Car.create(color: "blue", user: user_2)
+car_4 = Car.create(color: "pink", user: user_3)
+car_5 = Car.create(color: "yellow")
+
+user_1.cars << car_5
+car_5.user = user_1
+car_5.user.id = user_1.id
+car_5.save!
